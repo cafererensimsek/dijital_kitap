45a564bfaa4392860269b86d600ffeeb88e68772
Eren Şimşek <scafereren@gmail.com>
2020-09-22 19:19:36 +0300

    auth

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,21 +1,20 @@
+import 'package:dijital_kitap/screens/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Authentication with ChangeNotifier {
   bool _isSignIn = true;
   String _email;
   String _password;
+  User _user;
 
-  bool get isSignIn {
-    return _isSignIn;
-  }
+  bool get isSignIn => _isSignIn;
 
-  String get email {
-    return _email;
-  }
+  String get email => _email;
 
-  String get password {
-    return _password;
-  }
+  String get password => _password;
+
+  User get currentUser => _user;
 
   void switchAuth() {
     _isSignIn = !_isSignIn;
@@ -30,5 +29,68 @@ class Authentication with ChangeNotifier {
   void changePassword(String newPassword) {
     _password = newPassword;
     notifyListeners();
+  }
+
+  void changeUser(User user) => _user = user;
+
+  void signIn(BuildContext ctx) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      changeUser(FirebaseAuth.instance.currentUser);
+      Navigator.of(ctx).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (_) => Home(),
+        ),
+        (route) => false,
+      );
+    } catch (e) {
+      Scaffold.of(ctx).showSnackBar(
+        SnackBar(
+          behavior: SnackBarBehavior.floating,
+          content: Row(
+            children: [
+              Icon(Icons.error),
+              SizedBox(width: 30),
+              Flexible(
+                  child: Text(e.message != null ? e.message : e.toString())),
+            ],
+          ),
+        ),
+      );
+    }
+  }
+
+  void signUp(BuildContext ctx) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      changeUser(FirebaseAuth.instance.currentUser);
+      Navigator.of(ctx).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (_) => Home(),
+        ),
+        (route) => false,
+      );
+    } catch (e) {
+      Scaffold.of(ctx).showSnackBar(
+        SnackBar(
+          behavior: SnackBarBehavior.floating,
+          content: Row(
+            children: [
+              Icon(Icons.error),
+              SizedBox(width: 30),
+              Flexible(
+                  child: Text(e.message != null ? e.message : e.toString())),
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
--- lib/widgets/auth/auth_button.dart
+++ lib/widgets/auth/auth_button.dart
@@ -1,87 +1,23 @@
-import 'package:dijital_kitap/screens/first_time.dart';
-import 'package:dijital_kitap/screens/home.dart';
-
 import '../../providers/auth.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class AuthButton extends StatelessWidget {
-  void _signIn(String email, String password, BuildContext ctx) async {
-    try {
-      await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      User user = FirebaseAuth.instance.currentUser;
-      Navigator.of(ctx).pushAndRemoveUntil(
-        MaterialPageRoute(
-          builder: (_) => Home(),
-        ),
-        (route) => false,
-      );
-    } catch (e) {
-      Scaffold.of(ctx).showSnackBar(
-        SnackBar(
-          behavior: SnackBarBehavior.floating,
-          content: Row(
-            children: [
-              Icon(Icons.error),
-              SizedBox(width: 30),
-              Flexible(
-                  child: Text(e.message != null ? e.message : e.toString())),
-            ],
-          ),
-        ),
-      );
-    }
-  }
-
-  void _signUp(String email, String password, BuildContext ctx) async {
-    try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      User user = FirebaseAuth.instance.currentUser;
-      Navigator.of(ctx).pushAndRemoveUntil(
-        MaterialPageRoute(
-          builder: (_) => Home(),
-        ),
-        (route) => false,
-      );
-    } catch (e) {
-      Scaffold.of(ctx).showSnackBar(
-        SnackBar(
-          behavior: SnackBarBehavior.floating,
-          content: Row(
-            children: [
-              Icon(Icons.error),
-              SizedBox(width: 30),
-              Flexible(
-                  child: Text(e.message != null ? e.message : e.toString())),
-            ],
-          ),
-        ),
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
     final double height = MediaQuery.of(context).size.height;
     bool isSignIn = Provider.of<Authentication>(context).isSignIn;
-    String _email = Provider.of<Authentication>(context).email;
-    String _password = Provider.of<Authentication>(context).password;
     return Positioned(
       left: 20 * width / 24,
       top: 80 * height / 100,
       child: IconButton(
         icon: Icon(Icons.arrow_forward_ios, color: Colors.white70, size: 30),
         onPressed: isSignIn
-            ? () => _signIn(_email, _password, context)
-            : () => _signUp(_email, _password, context),
+            ? () => Provider.of<Authentication>(context, listen: false)
+                .signIn(context)
+            : () => Provider.of<Authentication>(context, listen: false)
+                .signUp(context),
       ),
     );
   }
